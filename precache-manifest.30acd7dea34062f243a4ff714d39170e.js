4215c772572169589b7fe62f8986d73be95f7519
yvvsra <yvvsra1@gmail.com>
2020-05-18 21:01:14 +0300

    Updates

---
+++ precache-manifest.30acd7dea34062f243a4ff714d39170e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ce522c7b3e65a90201519313c44d10",
+    "url": "/portfoi/index.html"
+  },
+  {
+    "revision": "27b3747fc14acacf8f58",
+    "url": "/portfoi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cec84e4499ccb869daff",
+    "url": "/portfoi/static/js/2.dc1d6c6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfoi/static/js/2.dc1d6c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b3747fc14acacf8f58",
+    "url": "/portfoi/static/js/main.eb77b915.chunk.js"
+  },
+  {
+    "revision": "b1f6084908c89d3f843e",
+    "url": "/portfoi/static/js/runtime-main.af312044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfoi/static/media/logo.5d5d9eef.svg"
+  }
+]);
